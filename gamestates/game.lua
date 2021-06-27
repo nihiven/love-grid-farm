da0ddd4ea1ad58487e7edbaf0388a6e80fec8851
Justin Behanna <j.behanna@gmail.com>
2021-06-27 14:02:34 -0400

    enable grid movement with keyboard

--- gamestates/game.lua
+++ gamestates/game.lua
@@ -1,3 +1,5 @@
+-- JUICE: shake the grid when plot changes
+
 --- HELPER FUNCTIONS ----
 function _log(message)
   -- check for the log object, print otherwise
@@ -13,6 +15,7 @@ function rgbtolove(r, g, b, a)
 end
 
 
+--- GAME OBJECT ---
 local game = {
   -- gamestate related
   _previous = nil,
@@ -39,7 +42,6 @@ local game = {
       }
     }
   },
-
   -- plot data
   _plots = {
     -- plot state goes in here
@@ -48,8 +50,8 @@ local game = {
 
   -- the active plot
   _active = {
-    x = nil,
-    y = nil,
+    x = 0,
+    y = 0,
     color = {1,0,1}
   },
 
@@ -61,11 +63,15 @@ local game = {
 
   -- mouse related
   _mouse = {
+    paused = false,
     x = 0,
     y = 0,
+    -- change since last update
     dx = 0,
     dy = 0,
-    isTouch = false
+    -- relative to the grid
+    rx = 0,
+    ry = 0,
   },
 
   -- debug settings
@@ -78,28 +84,29 @@ local game = {
   }
 }
 
--- NEXT: arrows change active plot
--- JUICE: shake the grid when plot changes
-local cursor = love.mouse.newCursor(love.image.newImageData("pointer.png"), 0, 0)
+-- TODO: holding down keys doesn't do anything
+local cursor = love.mouse.newCursor(love.image.newImageData("images/pointer.png"), 0, 0)
 
 function game:drawStats(stats)
-  -- stats are updated at the end of game:draw()
   local wwidth, wheight = love.graphics.getWidth(), love.graphics.getHeight()
 
+  -- if draw flag is set and passed stats aren't nil
   if (self._debug.draw and stats ~= nil) then
     local str = string.format(
-      "Draw Calls: %i\nTexture Memory: %.2f MB\nMouse [x:%i y:%i] [rx:%i ry:%i]\nPlot: %i,%i",
+      "Draw Calls: %i | Texture Memory: %.2f MB\nMouse [x:%i y:%i] [rx:%i ry:%i] | Paused: %s\nPlot: %i,%i",
       stats.drawcalls,
       stats.texturememory / 1024 / 1024,
       self._mouse.x,
       self._mouse.y,
       self._mouse.rx,
       self._mouse.ry,
+      self._mouse.paused,
       self._active.x and self._active.x or -1,
       self._active.y and self._active.y or -1
     )
-    local sheight = self._debug.font:getHeight(str)
 
+    -- get the string height to use when setting origin y
+    local sheight = self._debug.font:getHeight(str)
     love.graphics.setColor(self._debug.color)
     love.graphics.print(str, self._debug.padx, wheight - sheight - self._debug.pady)
   end
@@ -131,7 +138,6 @@ end
 function game:drawPlots(refreshCanvas)
   if (self._canvas.data == nil or refreshCanvas) then
     -- there isn't a canvas OR we're forcing a refresh
-    prinspect(refreshCanvas)
     print(refreshCanvas and 'refreshing grid canvas...' or 'creating grid canvas...')
 
     -- create canvas
@@ -173,13 +179,18 @@ function game:drawPlots(refreshCanvas)
 
     -- change color and draw the highlight
     love.graphics.setColor(self._active.color)
-    love.graphics.rectangle('line', ax, ay, self._plot.width, self._plot.height)
+    love.graphics.rectangle('fill', ax, ay, self._plot.width, self._plot.height)
   end
 end
 
 function game:updateMouse()
+  local ox, oy = self._mouse.x, self._mouse.y
   local x, y = love.mouse.getPosition() -- get the position of the mouse
   
+  -- mouse coords didn't change, return false
+  if (ox == x and oy == y) then
+    return false
+  end
   -- window relative
   self._mouse.x = x
   self._mouse.y = y
@@ -187,27 +198,24 @@ function game:updateMouse()
   -- grid relative
   self._mouse.rx = x - self._plot.x
   self._mouse.ry = y - self._plot.y
+
+  -- unpause mouse
+  self:unpauseMouse()
+  return true
 end
 
 -- returns bool - did plot change
-function game:updateActivePlot()
+function game:updateActivePlotFromMouse()
   -- store old values
   local ox, oy = self._active.x, self._active.y 
 
-  -- check to see if the active plot is within bounds
+  -- check to see if the active plot is within bounds, 
+  -- use grid relative values (rx, ry), store new values
   local nx, ny = self:getPlot(self._mouse.rx, self._mouse.ry)
 
-  -- confine the active plot to the grid on the x axis
-  if (nx >= 0 and nx < self._plot.count) then
-    self._active.x = nx
-    plotChanged = true
-  end
-
-  -- confine the active plot to the grid on the y axis
-  if (ny >= 0 and ny < self._plot.count) then
-    self._active.y = ny
-    plotChanged = true
-  end
+  -- confine the active plot to the grid on the x axis, store new values
+  self._active.x = clamp(0, nx or 0, self._plot.count-1) -- -1 for zero index
+  self._active.y = clamp(0, ny or 0, self._plot.count-1) -- -1 for zero index
 
   -- check to see if the old plot values are different than the new ones
   if (self._active.x == ox and self._active.y == oy) then
@@ -230,14 +238,64 @@ function game:getPlot(x, y)
   return math.floor(x / self._plot.width), math.floor(y / self._plot.height)
 end
 
+-- plot selection via keypress
+-- x,y are number of plots to move in each direction
+-- returns the x,y delta
+function game:movePlot(x, y)
+  -- NOTE: we need to disable the mouse until it moves again
+  -- NOTE: otherwise it will immediately override movePlot results
+  -- REVIEW: we might not want to pause the mouse if the keypress has no effect
+  local ox, oy = self._active.x, self._active.y
+
+  -- move based on plot movement
+  local nx = clamp(0, self._active.x + (x or 0), self._plot.count-1)
+  local ny = clamp(0, self._active.y + (y or 0), self._plot.count-1)
+
+  -- store the x,y delta
+  local dx, dy = nx - ox, ny - oy
+
+  -- update _active properties
+  self._active.x = self._active.x + dx
+  self._active.y = self._active.y + dy
+
+  -- update _plots properties
+  self._plots[ox][oy].selected = false
+  self._plots[self._active.x][self._active.y].selected = true
+
+  if (dx ~= 0 or dy ~= 0) then
+    -- REVIEW: play sound is now in two places
+    love.audio.play(self._plot.sounds.grid.change)
+    
+    -- pause mouse updates
+    self:pauseMouse() 
+  end
+
+  return dx, dy
+end
+
+function game:pauseMouse()
+  if (not self._mouse.paused) then
+    self._mouse.paused = true
+    print('mouse paused')
+  end
+end
+
+function game:unpauseMouse()
+  if (self._mouse.paused) then
+    self._mouse.paused = false
+    print('mouse unpaused')
+  end
+end
 
 ---- LOVE CALLBACKS ----
 function game:update(dt)
-  self:updateMouse()
-
-  -- returns true if the active plot changed
-  if (self:updateActivePlot() == true) then
-    love.audio.play(self._plot.sounds.grid.change)
+  -- REVIEW: does updateActivePlot move into updateMouse??
+  -- returns true if mouse moved
+  if (self:updateMouse()) then
+    -- returns true if the active plot changed from mouse movement
+    if (self:updateActivePlotFromMouse() == true) then
+      love.audio.play(self._plot.sounds.grid.change)
+    end
   end
 end
 
@@ -250,8 +308,19 @@ function game:keypressed(key)
   local keys = {
     s = function() print(inspect(self._graphicsStats)) end,
     u = function() prinspect(self._active) end,
-    x = function() print('jeb hit x') end,
     p = function() prinspect(self._plots[self._active.x][self._active.y]) end,
+    
+    -- arrow movement
+    up = function() self:movePlot(0,-1) end,
+    down = function() self:movePlot(0,1) end,
+    left = function() self:movePlot(-1,0) end,
+    right = function() self:movePlot(1,0) end,
+
+    -- wasd movement
+    w = function() self:movePlot(0,-1) end,
+    s = function() self:movePlot(0,1) end,
+    a = function() self:movePlot(-1,0) end,
+    d = function() self:movePlot(1,0) end,
 
     -- REVIEW: menus need some work
     escape = function() gamestate.push(self._previous) end
--- system.lua
+++ system.lua
@@ -10,3 +10,11 @@ local function callfuncontable(objects, func, ...)
 		value[func](...)
 	end
 end
+
+-- Clamps a value to a certain range.
+-- @param min - The minimum value.
+-- @param val - The value to clamp.
+-- @param max - The maximum value.
+function clamp(min, val, max)
+    return math.max(min, math.min(val, max));
+end
